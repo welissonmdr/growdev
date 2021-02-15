4d3fef7afc9027fece5ab72a4f8feb442a37355d
Welisson Rocha <welissonrocha717@gmail.com>
2021-02-15 17:42:31 -0300

    primeiro commit

---
+++ Codai/Js/index.js
@@ -0,0 +1,34 @@
+var nome = "Welisson Rocha";
+var cargo = "Desenvolvedor";
+
+var nomeHtml = document.getElementById("nome-no-html");
+var cargoHtml = document.getElementById("cargo-no-html");
+var texto1 = document.getElementById("texto-1");
+var texto2 = document.getElementById("texto-2");
+
+function colocarNomeNoHtml(nome) {
+    nomeHtml.innerHTML = nome;
+}
+
+function colocarCargoNoHtml(cargo) {
+  cargoHtml.innerHTML = cargo;
+}
+
+function LogarNome() {
+    console.log(nome);
+}
+
+function clickNoProjetos() {
+    console.log("Clicou no botão projetos"); 
+    texto2.style.display = "block";
+    texto1.style.display = "none";
+}
+
+function clickNoSobre(){
+    console.log('Clicou no botão sobre')
+    texto1.style.display = "block";
+    texto2.style.display = "none";
+}
+
+colocarNomeNoHtml(nome);
+colocarCargoNoHtml(cargo);
